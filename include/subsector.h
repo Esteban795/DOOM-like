def9917a353eb1649f479757bad8ae9fb2baf1cb
Esteban MANDJUKOWSKI <estebanmandjukowski@gmail.com>
2024-02-08 02:04:55 +0100

    feat(subsector) : [+] subsector struct and read subsector and subsectors from WAD file

---
+++ include/subsector.h
@@ -0,0 +1,17 @@
+#ifndef SUBSECTOR_H
+#define SUBSECTOR_H
+
+#include "byte_reader.h"
+#include "lump.h"
+
+// https://doomwiki.org/wiki/Subsector
+
+struct Subsector {
+    i16 num_segs;
+    i16 first_seg_id;
+};
+
+typedef struct Subsector subsector;
+
+subsector* get_subsectors_from_lump(FILE* f,lump* directory,int lump_index, int num_bytes,int header_length,int len_subsectors);
+#endif
